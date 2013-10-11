599be349de26d95f9035671c779b2dc8c6f3da60
David Laing <david@davidlaing.com>
2013-10-11 13:37:40 +0100

    Include quick links to common commands

--- src/web-ui/app/scripts/controllers/main.js
+++ src/web-ui/app/scripts/controllers/main.js
@@ -7,12 +7,15 @@ angular.module('containerInfoWebUiApp')
 
     $scope.execute_command = function(cmd) {
     	var url = "/exec?cmd="+cmd;
+
     	$http( { method: 'GET', url: url } )
 			  .success(function(data, status, headers, config) {
+			  	$scope.cmd = cmd;
 			    $scope.cmd_results = data;
 			  })
 			  .error(function(data, status, headers, config) {
-			     $scope.cmd_results = "Error: status:" + status + ", data:"+data;
+			  	$scope.cmd = cmd;
+			    $scope.cmd_results = "Error: status:" + status + ", data:"+data;
 			  });
 		
 		}
