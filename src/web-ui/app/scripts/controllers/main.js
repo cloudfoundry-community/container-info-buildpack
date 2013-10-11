f2bfdc9a048eae8ae5ae4ecd05cc8638dfd79376
David Laing <david@davidlaing.com>
2013-10-11 12:22:38 +0100

    Execute remote commands

--- src/web-ui/app/scripts/controllers/main.js
+++ src/web-ui/app/scripts/controllers/main.js
@@ -1,10 +1,19 @@
 'use strict';
 
 angular.module('containerInfoWebUiApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $http) {
+
+    $scope.cmd_results = "... remote command results go here ..."
+
+    $scope.execute_command = function(cmd) {
+    	var url = "/exec?cmd="+cmd;
+    	$http( { method: 'GET', url: url } )
+			  .success(function(data, status, headers, config) {
+			    $scope.cmd_results = data;
+			  })
+			  .error(function(data, status, headers, config) {
+			     $scope.cmd_results = "Error: status:" + status + ", data:"+data;
+			  });
+		
+		}
   });
