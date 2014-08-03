43f5f3a30aa491d07f3e3964692d83c87c658f5d
arthur halet <arthurh.halet@gmail.com>
2014-08-03 23:36:36 +0200

    IHM for staging emulation

--- src/web-ui/app/scripts/app.js
+++ src/web-ui/app/scripts/app.js
@@ -1,13 +1,17 @@
 'use strict';
 
 angular.module('containerInfoWebUiApp', [])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .when('/buildpack', {
+                templateUrl: 'views/buildpack.html',
+                controller: 'MainCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
--- src/web-ui/app/scripts/controllers/main.js
+++ src/web-ui/app/scripts/controllers/main.js
@@ -1,22 +1,27 @@
 'use strict';
 
 angular.module('containerInfoWebUiApp')
-  .controller('MainCtrl', function ($scope, $http) {
+    .controller('MainCtrl', function ($scope, $http) {
 
-    $scope.cmd_results = "... remote command results go here ..."
+        $scope.cmd_results = "... remote command results go here ..."
 
-    $scope.execute_command = function(cmd) {
-    	var url = "/exec?cmd="+cmd;
+        $scope.execute_command = function (cmd, url) {
+            if (url == undefined) {
+                var url = "/exec?cmd=" + cmd;
+            } else {
+                url += cmd;
+            }
 
-    	$http( { method: 'GET', url: url } )
-			  .success(function(data, status, headers, config) {
-			  	$scope.cmd = cmd;
-			    $scope.cmd_results = data;
-			  })
-			  .error(function(data, status, headers, config) {
-			  	$scope.cmd = cmd;
-			    $scope.cmd_results = "Error: status:" + status + ", data:"+data;
-			  });
-		
-		}
-  });
+
+            $http({ method: 'GET', url: url })
+                .success(function (data, status, headers, config) {
+                    $scope.cmd = cmd;
+                    $scope.cmd_results = data;
+                })
+                .error(function (data, status, headers, config) {
+                    $scope.cmd = cmd;
+                    $scope.cmd_results = "Error: status:" + status + ", data:" + data;
+                });
+
+        }
+    });
